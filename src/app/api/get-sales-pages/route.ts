a0845402c979d6a1a53b9655a57a173cbad8d6ed
Yoshiaki Shibutani <yoshiaki.shibutani@fidx.co.jp>
2025-02-16 10:37:05 +0900

    fix: eslint error

--- src/app/api/get-sales-pages/route.ts
+++ src/app/api/get-sales-pages/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server'
 const prisma = new PrismaClient()
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function GET(_request: NextRequest) {
+export async function GET(_: NextRequest) {
   try {
     const salesPages = await prisma.salesPage.findMany({
       orderBy: {
